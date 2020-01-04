70487ecd7c301067aac2d05a216c568e84c6d8b7
Joel Ricci-Lopez <ricci.lob@gmail.com>
2020-01-03 22:24:17 -0800

    First part on bokeh server

---
+++ Section_4/bokeh_server.py
@@ -0,0 +1,31 @@
+# herein we are running abokeh server
+# how to create widgets with bokeh
+
+# importing libraries
+from bokeh.plotting import show
+from bokeh.io import curdoc # output_file
+from bokeh.models.widgets import TextInput, Button, Paragraph
+from bokeh.layouts import layout
+
+# prepare the bokeh output file
+# output_file('simple_bokeh.html') ## We don't need it if we use bokeh server
+
+# Create widgets
+text_input = TextInput(value = 'Perro')
+button = Button(label = 'Generar texto')
+# New paragraph to be updated
+output = Paragraph()
+
+# Update function
+def update():
+    output.text = F'Hello, {text_input.value}'
+
+button.on_click(update)
+
+_layout = layout([[button, text_input], [output]])
+
+# Showing function
+## show(_layout) ## We don't need it if we use bokeh server
+
+# now with bokeh server
+curdoc().add_root(_layout)
