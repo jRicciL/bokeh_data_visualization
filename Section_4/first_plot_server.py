0555f72b361f4f3a913977a80dcf1d8ebe54e701
Joel Ricci-Lopez <ricci.lob@gmail.com>
2020-01-09 18:22:02 -0800

    Firs plot server updated: axis and tittle

--- Section_4/first_plot_server.py
+++ Section_4/first_plot_server.py
@@ -5,14 +5,27 @@ from bokeh.plotting import figure, curdoc
 from bokeh.palettes import Spectral4
 from bokeh.sampledata.stocks import AAPL, IBM, MSFT, GOOG
 
-p = figure(plot_width = 800, plot_height = 250, x_axis_type = 'datetime')
-p.title.text = 'First Plot with Bokeh'
+p = figure(plot_width = 1200, plot_height = 400, x_axis_type = 'datetime')
+p.title.text = 'First Plot with Bokeh Server'
 p.title.align = 'center'
+p.title.text_font_size = '2em'
+p.axis.axis_line_width = 3
+p.axis.major_tick_line_width = 6
+
+# Text
+p.axis.axis_label_text_font_size = '1.5em'
+p.axis.axis_label_text_font_style = 'bold'
+p.axis.major_label_text_font_size = '1.2em'
+p.xaxis.axis_label = 'X axis'
+p.yaxis.axis_label = 'Year'
+
+# Responsive plot
+p.sizing_mode = 'scale_both'
 
 for data, name, color in zip([AAPL, IBM, MSFT, GOOG],  ["AAPL", "IBM", "MSFT", "GOOG"], Spectral4):
     df = pd.DataFrame(data)
     df['date'] = pd.to_datetime(df['date'])
-    p.line(df['date'], df['close'], line_width = 2,
+    p.line(df['date'], df['close'], line_width = 3,
             color = color, alpha = 0.8, legend = name,
             muted_color=color, muted_alpha=0.1)
 
