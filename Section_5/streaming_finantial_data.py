005ea427eed7296481034b6867eac6e96f757e32
Joel Ricci-López- <ricci.lob@gmail.com>
2020-02-10 12:24:06 -0800

    Web scrapping and plotting finished. style needed

--- Section_5/streaming_finantial_data.py
+++ Section_5/streaming_finantial_data.py
@@ -1,7 +1,49 @@
 import pandas as pd 
 import numpy as np 
 
+import requests
+from bs4 import BeautifulSoup
+
 from bokeh.plotting import figure, curdoc
 from bokeh.models import ColumnDataSource
 
+# Function for webscrapping
+URL = 'https://bitcoincharts.com/markets/bitstampUSD.html'
+HEADER = {'User-Agent': 'Mozilla/5.0'}
+
+def scrap_value():
+    r = requests.get(URL, headers = HEADER)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    print(soup.find_all('div', {'id': 'market_summary'})[0].span.text)
+    last_trade = float(soup.find_all('div', 
+    {'id': 'market_summary'})[0].span.text)
+    return(last_trade)
+
+source = ColumnDataSource({
+    'x': [1.0],
+    'y': [scrap_value()]
+})
+
 f = figure()
+# Create the glyphs
+f.circle(x='x', y='y', color='cyan', line_color = 'blue',
+source=source)
+# We can use different glyphs
+f.line(x = 'x', y = 'y', source = source)
+
+
+# Create periodic function
+def update():
+    # updates the data
+    y = scrap_value()
+    # x is the last x data
+    x = source.data['x'][-1] + 1
+    new_data = {
+        # Create random points
+        'x': [x],
+        'y': [y],
+    }
+    source.stream(new_data, rollover = 100)
+
+curdoc().add_root(f)
+curdoc().add_periodic_callback(update, 2000)
